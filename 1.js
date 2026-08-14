207d036f6c4f749899dca4d9a9dbd1687fa0b698
Prithvi <prithvirajpatil64@gmail.com>
2026-08-14 06:49:52 +0530

    JS Intermediate Question 1 done

--- 1.js
+++ 1.js
@@ -5,3 +5,17 @@ let users = [
     { user: "B", amount: 200 },
     { user: "A", amount: 50 }
 ]
+
+let sumAmounts = {};
+
+for(let i=0; i<users.length; i++) {
+    // console.log(users[i]);
+    // console.log(users[i].user)
+    if(!sumAmounts[users[i].user]) {
+        sumAmounts[users[i].user] = users[i].amount;
+    } else {
+        sumAmounts[users[i].user] += users[i].amount;
+    }
+}
+
+console.log(sumAmounts);
