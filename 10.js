237bae9b35c2214fd1d583e3391e121c479ba3bd
Prithvi <prithvirajpatil64@gmail.com>
2026-08-19 09:11:25 +0530

    JS Intermediate Question 10 done

--- 10.js
+++ 10.js
@@ -1,1 +1,30 @@
 // Remove deeply nested key from object
+
+let obj = { a: { b: { c: 1, d: 2 } } };
+
+// for(let key in obj) {    //This solution is correct for given input, but doesn't work for depth
+//     // console.log(key);
+//     // console.log(obj[key]);
+//     for(innerKey in obj[key]) {
+//         // console.log(innerKey);
+//         // console.log(obj[key][innerKey]);
+//         delete(obj[key][innerKey].c);
+//     }
+// }
+
+// console.log(obj);
+
+function removeKey(obj, keyToRemove) {
+    for(let key in obj) {
+        if(key === keyToRemove) {
+            delete obj[key];
+        } 
+        else if(typeof obj[key] === "object") {
+            removeKey(obj[key], keyToRemove);
+        }
+    }
+}
+
+removeKey(obj, "c");
+
+console.log(obj);
